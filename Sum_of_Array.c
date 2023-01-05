9b32e36a00623d6dd14087034d49fdd66791b765
LOSETTY . S N A SWAMY <119719236+SURYA5001@users.noreply.github.com>
2023-01-05 09:53:48 +0530

    #include<stdio.h> int main() {    int N,i,s=0;    scanf("%d",&N);    int a[N];    for(i=0;i<N;i++)    {        scanf("%d",&a[i]);    }    for(i=0;i<N;i++)    {        s=s+a[i];    }    printf("%d",s); }

--- Sum_of_Array.c
+++ Sum_of_Array.c
@@ -1,16 +1,16 @@
 #include<stdio.h>
 int main()
 {
-    int N;
-    scanf("%d",&N);
-    int a[N],i,s=0;
-    for(i=0;i<N;i++)
-    {
-        scanf("%d",&a[i]);
-    }
-    for(i=0;i<N;i++)
-    {
-    s=s+a[i];
-    }
-        printf("%d",s);
+   int N,i,s=0;
+   scanf("%d",&N);
+   int a[N];
+   for(i=0;i<N;i++)
+   {
+       scanf("%d",&a[i]);
+   }
+   for(i=0;i<N;i++)
+   {
+       s=s+a[i];
+   }
+   printf("%d",s);
 }
